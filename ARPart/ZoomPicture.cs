979f6f07891f00d4931f95bf574f79d0a224fabe
khseob0715 <khseob0715@naver.com>
2017-09-16 16:19:53 +0900

    현재까지의 작업물 Merge Commit
    * 현재까지의 개인 작업물을 모두 합친 기준 제작.

---
+++ ARPart/ZoomPicture.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomPicture : MonoBehaviour
+{
+    public GameObject ZoominCell;
+    private Sprite ImageRef;
+    void Start()
+    {
+        ImageRef = gameObject.GetComponent<Image>().sprite;
+
+    }
+    public void SizeUpPicture()
+    {
+        var New = Instantiate<GameObject>(ZoominCell);
+        // 버튼 클릭시 UI에다 큰 이미지를 생성해준다.
+        New.GetComponent<Image>().sprite = ImageRef;
+        New.transform.SetParent(gameObject.transform.parent);
+        New.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        //그리고 그 UI을 다시 클릭하면 사라진다.
+
+    }
+    public void Delme()
+    {
+        DestroyImmediate(gameObject);
+    }
+}
